61c2faea6b545a35a17aa2119d9d1edb7c55d744
Akiyo Fidel <akiyofidel@gmail.com>
2020-05-13 18:39:29 +0300

    .

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class HelloMessage extends React.Component {
-
   render() {
-    const {name} = this.props
-    return <div>Hello {name}</div>;
+    const { name } = this.props;
+    return <div> Hello { name } </div>;
   }
 }
 
-ReactDOM.render(<HelloMessage name='TEAM 273' />, document.getElementById('app'));
+ReactDOM.render(<HelloMessage name = 'TEAM 273' />, document.getElementById('app'));
