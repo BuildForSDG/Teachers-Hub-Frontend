a413e124dd3b7d196d999646e4739f0cf64f52fb
Akiyo Fidel <akiyofidel@gmail.com>
2020-05-13 19:22:10 +0300

    added props validation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 class HelloMessage extends React.Component {
   render() {
@@ -7,5 +8,9 @@ class HelloMessage extends React.Component {
     return <div> Hello { name } </div>;
   }
 }
+
+HelloMessage.propTypes = {
+  name:PropTypes.string
+};
 
 ReactDOM.render(<HelloMessage name = 'TEAM 273' />, document.getElementById('app'));
