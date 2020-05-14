f5aebebbbf2508fc97e6341096a2e70861f5735c
Akiyo Fidel <akiyofidel@gmail.com>
2020-05-14 20:22:52 +0300

    added deoendancy for prop-tools and corrected codacy es6 style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 class HelloMessage extends React.Component {
   render() {
@@ -10,7 +10,7 @@ class HelloMessage extends React.Component {
 }
 
 HelloMessage.propTypes = {
-  name:PropTypes.string
+  name: PropTypes.string
 };
 
 ReactDOM.render(<HelloMessage name = "TEAM 273" />, document.getElementById('app'));
