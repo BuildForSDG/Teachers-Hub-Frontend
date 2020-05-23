e9b307a573f1d2479e0c4aec1b13bd3f624e95db
mariamiah <mariah.kakande@gmail.com>
2020-05-23 18:34:33 +0300

    setup landing page

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import {LandingPage} from './components/LandingPage';
+import './App.css'
+
+
 
 function App() {
-  return <div className="App">Team-273</div>;
+  return <div className="App"><LandingPage /></div>;
 }
 
 export default App;
