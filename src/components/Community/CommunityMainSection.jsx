8fe16374f053b5612c0ebf375d06fd8e7822585d
mariamiah <mariah.kakande@gmail.com>
2020-06-15 16:04:57 +0300

    add the 3 panel sections

--- src/components/Community/CommunityMainSection.jsx
+++ src/components/Community/CommunityMainSection.jsx
@@ -1,8 +1,21 @@
 import React from "react";
+import SidePanel from "./SidePanel.jsx";
+import MainPanel from "./MainPanel.jsx";
+import RightPanel from "./RightPanel.jsx";
 
 const CommunityMainSection = () => (
-    <div>
-   
+<div className="container">
+  <div className="row">
+    <div className="col-sm">
+        <SidePanel />
+    </div>
+    <div className="col-sm">
+        <MainPanel />
+    </div>
+    <div className="col-sm">
+        <RightPanel />
+    </div>
+  </div>
   </div>
 );
 
---
+++ src/components/Community/SidePanel.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SidePanel = () => (
+        <div>
+            Side Panel
+        </div>
+);
+export default SidePanel;
