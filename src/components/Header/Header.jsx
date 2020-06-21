525b0500d80e201576802ccc061884b701ee0e77
mariamiah <mariah.kakande@gmail.com>
2020-06-21 18:10:16 +0300

    fix nav bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,33 +2,65 @@
 import React from "react";
 
 const Header = (props) => (
-        <header className='site-navbar py-4 js-sticky-header site-navbar-target' role='banner'>
-                <div className='container-fluid'>
-                    <div className='d-flex align-items-center'>
-                        <div className='site-logo mr-auto w-25'><a href={props.homeUrl}>Teachers Hub</a></div>
-                        <div className='mx-auto text-center'>
-                            <nav className='site-navigation position-relative text-right' role='navigation'>
-                                <ul className='site-menu main-menu js-clone-nav mx-auto d-none d-lg-block  m-0 p-0'>
-                                    <li><a href='#home-section' className='nav-link'>Home</a></li>
-                                    <li><a href='#courses-section' className='nav-link'>Courses</a></li>
-                                    <li><a href='#blogs-section' className='nav-link'>Blog</a></li>
-                                    <li><a href='#partners-section' className='nav-link'>Partnerships</a></li>
-                                    <li><a href='#community-section' className='nav-link'>Community</a></li>
-                                </ul>
-                            </nav>
-                        </div>
-                        <div className='ml-auto w-25'>
-                            <nav className='site-navigation position-relative text-right' role='navigation'>
-                                <ul className='site-menu main-menu site-menu-dark js-clone-nav mr-auto d-none d-lg-block m-0 p-0'>
-                                    <li className='cta'><a href={props.url} className='nav-link'
-                                    onClick={() => (props.buttonValue === "LogOut" ? localStorage.removeItem("token") : null)}><span>{props.buttonValue}</span></a></li>
-                                </ul>
-                            </nav>
-                            <a href='/' className='d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black float-right'><span className='icon-menu h3'></span></a>
-                        </div>
-                    </div>
-                </div>
+  <header className="site-navbar py-4 js-sticky-header site-navbar-target" role="banner">
+    <div className="container-fluid">
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark navbar-custom">
+          <a className="navbar-brand site-logo mr-auto w-25" href={props.homeUrl}>
+            Teachers Hub
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
 
-            </header>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="#home-section">
+                  Home <span className="sr-only">(current)</span>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#courses-section" className="nav-link">
+                  Courses
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#blogs-section" className="nav-link">
+                  Blog
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#partners-section" className="nav-link">
+                  Partnerships
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#community-section" className="nav-link">
+                  Community
+                </a>
+              </li>
+            </ul>
+            <a href={props.url}>
+              <input
+                type="submit"
+                className="btn btn-primary my-2 my-sm-0"
+                onClick={() => (props.buttonValue === "LogOut" ? localStorage.removeItem("token") : null)}
+                value={props.buttonValue}
+              />
+            </a>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </header>
 );
 export default Header;
--- src/components/SingleCourse/SingleCourse.jsx
+++ src/components/SingleCourse/SingleCourse.jsx
@@ -12,70 +12,86 @@ export const SingleCourse = (props) => {
   const isAuthenticated = localStorage.getItem("token");
   return (
     <div>
-{isAuthenticated ? <Header buttonValue="LogOut" url="/" homeUrl="/admin"/>
-  : <Header buttonValue="Login" url="/login" homeUrl="/"/>}
-    <div className="intro-section single-cover" id="home-section">
-      <div className="slide-1 " style={{ backgroundImage: "url(images/img_2.jpg)" }} data-stellar-background-ratio="0.5">
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col-12">
-            <div className="row justify-content-center align-items-center text-center">
-              <div className="col-lg-6">
-                <h1>{props.data.data.course
-                  ? capitalize(props.data.data.course.course_title) : null}</h1>
-                <button type="button" className="btn btn-default">ENROLL TO GET STARTED</button>
+      {isAuthenticated ? (
+        <Header buttonValue="LogOut" url="/" homeUrl="/admin" />
+      ) : (
+        <Header buttonValue="Login" url="/login" homeUrl="/" />
+      )}
+      <div className="intro-section single-cover" id="home-section">
+        <div
+          className="slide-1 "
+          style={{ backgroundImage: "url(images/img_2.jpg)" }}
+          data-stellar-background-ratio="0.5"
+        >
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-12">
+                <div className="row justify-content-center align-items-center text-center">
+                  <div className="col-lg-6">
+                    <h1>{props.data.data.course ? capitalize(props.data.data.course.course_title) : null}</h1>
+                    <button type="button" className="btn btn-default">
+                      ENROLL TO GET STARTED
+                    </button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      </div>
-      </div>
       <div className="site-section">
-      <div className="container">
-      <div className="row">
-        <div className="col-lg-8 mb-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 mb-5">
+              <div className="mb-5">
+                <h3 className="text-black">Course Description</h3>
+                <p>{props.data.data.course ? props.data.data.course.course_description : null}</p>
 
-          <div className="mb-5">
-            <h3 className="text-black">Course Description</h3>
-                <p>{props.data.data.course
-                  ? (props.data.data.course.course_description) : null}</p>
-
-            <p className="mt-4"><a href="#" className="btn btn-primary">Enroll</a></p>
-          </div>
-          {props.modules.data && props.modules.data.message
-            ? props.modules.data.message.map((module) => (
-              <div key={module.module_id}>
-                <CourseExpansionPanel
-                id = {module.module_id}
-                course_id = {module.course_id}
-                module_date_added = {module.module_date_added}
-                module_description = {module.module_description}
-                module_title = {module.module_title}
-                />
+                <p className="mt-4">
+                  <a href="#" className="btn btn-primary">
+                    Enroll
+                  </a>
+                </p>
+              </div>
+              {props.modules.data && props.modules.data.message
+                ? props.modules.data.message.map((module) => (
+                    <div key={module.module_id}>
+                      <CourseExpansionPanel
+                        id={module.module_id}
+                        course_id={module.course_id}
+                        module_date_added={module.module_date_added}
+                        module_description={module.module_description}
+                        module_title={module.module_title}
+                      />
+                    </div>
+                  ))
+                : null}
+              <div className="pt-5">
+                <CommentsContainer courseId={props.course_id} />
+              </div>
+            </div>
+            <div className="col-lg-4 pl-lg-5">
+              <div className="mb-5 text-center border rounded course-instructor">
+                <h3 className="mb-5 text-black text-uppercase h6 border-bottom pb-3">Course Instructor</h3>
+                <div className="mb-4 text-center">
+                  <img
+                    src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                    alt="Image"
+                    className="w-25 rounded-circle mb-4"
+                  />
+                  <a href="#">
+                    <h3 className="h5 text-black mb-4">
+                      {props.data.data.course ? capitalize(props.data.data.course.course_instructor) : null}
+                    </h3>
+                  </a>
+                  <p>More than 5 years teaching experience having great mentorship abilities... .</p>
                 </div>
-            )) : null}
-          <div className="pt-5">
-           <CommentsContainer courseId={props.course_id} />
-          </div>
-        </div>
-        <div className="col-lg-4 pl-lg-5">
-
-          <div className="mb-5 text-center border rounded course-instructor">
-            <h3 className="mb-5 text-black text-uppercase h6 border-bottom pb-3">Course Instructor</h3>
-            <div className="mb-4 text-center">
-              <img src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="Image" className="w-25 rounded-circle mb-4" />
-              <a href="#"><h3 className="h5 text-black mb-4">{props.data.data.course
-                ? capitalize(props.data.data.course.course_instructor) : null}</h3></a>
-              <p>More than 5 years teaching experience having great mentorship abilities... .</p>
+              </div>
             </div>
           </div>
-
         </div>
       </div>
-      </div>
-      </div>
-          <Footer />
-      </div>
+      <Footer />
+    </div>
   );
 };
